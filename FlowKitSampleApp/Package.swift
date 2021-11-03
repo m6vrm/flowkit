5f9757199ede632824aa65db2a05f91dd63524ea
Roman Madyanov <romantaken@gmail.com>
2021-11-03 22:11:03 +0300

    Update Package.swift

--- FlowKitSampleApp/Package.swift
+++ FlowKitSampleApp/Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5
 
 import PackageDescription
 
@@ -7,36 +6,15 @@ let package = Package(
     name: "FlowKitSampleApp",
     platforms: [.iOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "NavigationKit",
-            targets: ["NavigationKit"]),
-        .library(
-            name: "PromiseKit",
-            targets: ["PromiseKit"]),
-        .library(
-            name: "FlowKit",
-            targets: ["FlowKit"]),
-        .library(
-            name: "AppFeature",
-            targets: ["AppFeature"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "NavigationKit", targets: ["NavigationKit"]),
+        .library(name: "PromiseKit", targets: ["PromiseKit"]),
+        .library(name: "FlowKit", targets: ["FlowKit"]),
+        .library(name: "AppFeature", targets: ["AppFeature"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "NavigationKit",
-            dependencies: []),
-        .target(
-            name: "PromiseKit",
-            dependencies: []),
-        .target(
-            name: "FlowKit",
-            dependencies: []),
+        .target(name: "NavigationKit"),
+        .target(name: "PromiseKit"),
+        .target(name: "FlowKit"),
         .target(
             name: "TransferFlowFeature",
             dependencies: ["NavigationKit", "FlowKit", "PromiseKit"]),
